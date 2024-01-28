6435bf15f12736eb79bfcb6a95dc00894cbb5e9b
Praj <rajbansiprajwol@gmail.com>
2024-01-28 14:07:21 +0545

    don

---
+++ Module-2/Day-1/first.py
@@ -0,0 +1,20 @@
+#list
+a=[1,2,3,4,5,"hello",True]
+print(type(a))
+
+#tuple
+b=(1,2,3,4,5,"hello",True)
+print(type(b))
+
+print(a[3])
+print(a[-3])
+
+#append means adding
+a.append("hi")
+print(a)
+print(a[-1])
+
+#removing element from last index
+a.pop()
+print(a)
+
